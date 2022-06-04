a7b35e1a6b4f1f47889bb3b2397b5facbfe3c7fa
jothamardel <ardelmbiplang@gmail.com>
2022-06-04 16:18:26 +0100

    chore: updating application

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [email, setEmail] = useState('') 
-  const [password, setPassword] = useState('') 
-  const [ users, setUsers] = useState([])
+  const [data, setData] = useState({})
+  const [message, setMessage] = useState({})
 
-  function onSubmitForm(event) {
-    event.preventDefault();
-    console.log(email)
-    console.log(password)
+  function updateState(event) {
+    const { name, value } = event.target;
+    const obj = {...data}
+    obj[name] = value
+    setData(obj);
   }
 
-  
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(res =>  res.json())
-    .then(data => setUsers(data))
+  function httpRegisterUser(event) {
+    event.preventDefault()
+    fetch('http://192.168.1.171:1050/register', {
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: 'post'
+    })
+    .then(response => response.json())
+    .then(data => setMessage(data))
     .catch(err => console.log(err))
-    
-  }, [])
-  
-  console.log(users);
-  return (
-    <div className="App">
-      {/* <form onSubmit={onSubmitForm}>
-        <input onChange={event => setEmail(event.currentTarget.value)} placeholder="Email"/> <br></br>
-        <input onChange={event => setPassword(event.currentTarget.value)} placeholder="Password" type="password"/><br></br>
-        <button type='submit'>Submit</button>
-      </form> */}
+  }
 
-      <ul style={{textAlign: 'start'}}>
-        {
-          users.map(item => <li key={item.id}>{item.name || item.title}</li>)
-        }
-      </ul>
+  return (
+    <>
+    <div className='App'>
+      <form className='' style={{paddingBottom: '2rem'}} onSubmit={httpRegisterUser}>
+        {message.successful && <p>{message.message}</p>}
+        <input className='form-control' name="fullName" onChange={updateState} placeholder='Enter fullname'/><br></br>
+        <input className='form-control' name="username" onChange={updateState} placeholder='Enter username'/><br></br>
+        <input className='form-control' name="phone" onChange={updateState} placeholder='Enter mobile no.'/><br></br>
+        <input className='form-control' name="email" onChange={updateState}  placeholder='Enter email' type="email"/><br></br>
+        <input className='form-control' name="password" onChange={updateState} placeholder='******' type="password"/><br></br>
+        <select className='form-control'name="gender" onChange={updateState}>
+          <option>select gender</option>
+          <option value="male">male</option>
+          <option value="female">female</option>
+          <option value="others">others</option>
+        </select><br></br>
+        <button type='submit' style={{padding: '1rem', width: '20rem'}}>submit</button>
+      </form>
     </div>
+    </>
   );
 }
 
 export default App;
+
+/**
+  {
+    fullName: "",
+    password: "",
+    email: "",
+    phone: "",
+    gender: "",
+    username: ""
+  }
+**/
